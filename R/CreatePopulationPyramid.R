8e371c93628c7b995d2cca9798d8b5fcd6df1a55
Gowtham Rao <rao@ohdsi.org>
2023-12-07 15:10:03 -0500

    Update CreatePopulationPyramid.R

--- R/CreatePopulationPyramid.R
+++ R/CreatePopulationPyramid.R
@@ -16,15 +16,19 @@ createPopulationPyramid <-
     checkmate::assertDataFrame(data, min.cols = 3)
     checkmate::assertCharacter(ageGroupCol, len = 1)
     checkmate::assertCharacter(genderCol, len = 1)
-    checkmate::assertCharacter(populationCol, len = 1)
+    checkmate::assertNumeric(data[[populationCol]], any.missing = FALSE, lower = 0)
     checkmate::assertCharacter(title, len = 1)
     
-    # Extract the first integer value from the ageGroupCol
-    data$ageGroupNumeric <-
+    # Extract the starting age from each age group and sort the data
+    data$startAge <-
       as.numeric(gsub(".*?(\\d+).*", "\\1", data[[ageGroupCol]]))
+    ageGroupsOrdered <- data |>
+      dplyr::arrange(startAge) |>
+      dplyr::pull(ageGroupCol) |>
+      unique()
     
-    # Sort data based on extracted numeric age groups for proper y-axis ordering
-    data <- data[order(data$ageGroupNumeric),]
+    data[[ageGroupCol]] <-
+      factor(data[[ageGroupCol]], levels = ageGroupsOrdered)
     
     # Create a ggplot bar chart
     plot <- ggplot2::ggplot(data,
@@ -32,16 +36,17 @@ createPopulationPyramid <-
                               x = !!rlang::sym(ageGroupCol),
                               fill = !!rlang::sym(genderCol),
                               y = ifelse(
-                                test = data[[genderCol]] == "Male",
-                                yes = -data[[populationCol]],
-                                no = data[[populationCol]]
+                                test = .data[[genderCol]] == "Male",
+                                yes = -as.numeric(.data[[populationCol]]),
+                                no = as.numeric(.data[[populationCol]])
                               )
                             )) +
       ggplot2::geom_bar(stat = "identity") +
+      ggplot2::scale_x_discrete() +
       ggplot2::scale_y_continuous(
         labels = function(x)
           format(abs(x), big.mark = ",", scientific = FALSE),
-        limits = max(data[[populationCol]], na.rm = TRUE) * c(-1, 1)
+        limits = max(abs(data[[populationCol]]), na.rm = TRUE) * c(-1, 1)
       ) +
       ggplot2::coord_flip() +
       ggplot2::theme_minimal() +
